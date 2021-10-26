05dfd648a88c3a18502a0d6ecea1d81d280b1927
vitor-ramalho <39497088+vitor-ramalho@users.noreply.github.com>
2021-10-26 14:00:13 -0300

    Adicionando alguns comentários

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -3,10 +3,8 @@ pragma solidity ^0.8.0;
 
 import "./libraries/Base64.sol";
 
-// NFT contract to inherit from.
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-// Helper functions OpenZeppelin provides.
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
@@ -23,18 +21,19 @@ contract MyEpicGame is ERC721 {
         uint attackDamage;
     }
 
-    //The TokenId is the NFTs unique identifier
+    //TokenID do NFT
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     CharacterAttributes[] defaultCharacters;
 
 
-    //we create a mapping from the nft's tokenId => that NFts Attributes;
+    //mapping do token do NFT => Que contem os atributos do mesmo NFT - Ele é armazenado na variável puclia nftHolderAttributes;
     mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
     event CharacterNFTMinted(address sender, uint256 tokenId, uint256 characterIndex);
     event AttackComplete(uint newBossHp, uint newPlayerHp);
 
+    //Definindo Estrutura do Chefão
     struct BigBoss {
         string name;
         string imageURI;
@@ -45,9 +44,10 @@ contract MyEpicGame is ERC721 {
 
     BigBoss public bigBoss;
 
-    //A mapping from an address => the NFTs tokenID. Giv eme an ez way to store the owner of the nft and reference
+    // mapping do address => que contém o ID dos NFTs daquele endereço - O objetivo é armazenar na variável nftHolders o dono do NFT e as referencias
     mapping(address => uint256) public nftHolders;
     
+    //Inicializa as variáveis de estado do smart contract
     constructor(
         string[] memory characterNames,
         string[] memory characterImageURIs,
@@ -57,12 +57,12 @@ contract MyEpicGame is ERC721 {
         string memory bossImageURI,
         uint bossHp,
         uint bossAttackDamage
-         // Below, you can also see I added some special identifier symbols for our NFT.
-        // This is the name and symbol for our token, ex Ethereum and ETH. I just call mine
-        // Heroes and HERO. Remember, an NFT is just a token!
+         //identificador especial para o NFT.
+        // onome e simbolo token, ex Ethereum and ETH.
     )
-    ERC721("Heroes", "HERO")
+    ERC721("Memes", "MEME")
     {
+        //inicializar estrutura do chefão
         bigBoss = BigBoss({
             name: bossName,
             imageURI: bossImageURI,
@@ -73,6 +73,7 @@ contract MyEpicGame is ERC721 {
 
         console.log("Done initializing boss %s w/ HP %s, img %s", bigBoss.name, bigBoss.hp, bigBoss.imageURI);
 
+        // loop passando por todos os personagens e salvando os valores no contrato para usá-los depois. 
         for(uint i = 0; i < characterNames.length; i += 1){
             defaultCharacters.push(CharacterAttributes({
                 characterIndex: i,
@@ -86,19 +87,18 @@ contract MyEpicGame is ERC721 {
             CharacterAttributes memory c = defaultCharacters[i];
             console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
         }
-        // I increment tokenIds here so that my first NFT has an ID of 1
+        // incremento para que o id do primeiro NFT seja 1.
         _tokenIds.increment();
     }
 
     function mintCharacterNFT(uint _characterIndex) external {
-        // Get current tokenId (starts at 1 since we incremented in the constructor).
+        // Pega o atual tokenId.
         uint newItemId = _tokenIds.current();
 
-        // The magical function! Assigns the tokenId to the caller's wallet address.
+        // Assina o ID do token na carteira cadastrada.
         _safeMint(msg.sender, newItemId);
 
-        // We map the tokenId => their character attributes. More on this in
-        // the lesson below.
+        // map tokenId => atributos do personagem. 
         nftHolderAttributes[newItemId] = CharacterAttributes({
             characterIndex: _characterIndex,
             name: defaultCharacters[_characterIndex].name,
@@ -150,32 +150,32 @@ contract MyEpicGame is ERC721 {
     }
 
     function attackBoss() public {
-        // Get the state of the player's NFT.
+        // armazena o estado do NFT do usuario
         uint256 nftTokenIdOfPlayer = nftHolders[msg.sender];
         CharacterAttributes storage player = nftHolderAttributes[nftTokenIdOfPlayer];
 
         console.log("\nPlayer w/ character %s about to attack. Has %s HP and %s AD", player.name, player.hp, player.attackDamage);
         console.log("Boss %s has %s HP and %s AD", bigBoss.name, bigBoss.hp, bigBoss.attackDamage);
 
-        // Make sure the player has more than 0 HP.
+        // valida se o personagem tem mais de 0 HP
         require (
             player.hp > 0,
             "Error: character must have HP to attack boss."
         );
 
-        // Make sure the boss has more than 0 HP.
+        // valida se o chefe tem mais de 0 HP.
         require (
             bigBoss.hp > 0,
             "Error: boss must have HP to attack boss."
         );
 
-        // Allow player to attack boss.
+        // permite o jogador atackar o chefe
         if(bigBoss.hp < player.attackDamage){
             bigBoss.hp = 0;
         } else {
             bigBoss.hp = bigBoss.hp - player.attackDamage;
         }
-        // Allow boss to attack player
+        // permite o chefe atacar o jogador
         if(player.hp < bigBoss.attackDamage){
             player.hp = 0;
         } else{
@@ -187,6 +187,7 @@ contract MyEpicGame is ERC721 {
         emit AttackComplete(bigBoss.hp, player.hp);
     }
 
+    //verifica se o usuario tem algum NFT
     function checkIfUserHasNFT() public view returns (CharacterAttributes memory) {
         uint256 userNftTokenId = nftHolders[msg.sender];
 
@@ -198,10 +199,12 @@ contract MyEpicGame is ERC721 {
         }
     }
 
+    //Busca todos os personagens padrão
     function getAllDefaultCharacters() public view returns (CharacterAttributes[] memory){
         return defaultCharacters;
     }
 
+    // retorna o Chefe
     function getBigBoss() public view returns (BigBoss memory){
         return bigBoss;
     }
